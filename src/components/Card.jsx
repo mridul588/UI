d990ad89ed4957d0d9588194e1435ba1b188b868
Harsh Lohana <harshylohana15@gmail.com>
2023-04-22 17:00:41 +0530

    initial commit

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-const Card = ({ img, boxes }) => {
-  return (
-    <div className="max-w-sm p-3 bg-gray-700 border border-black rounded-lg shadow flex flex-col items-center">
-      <img src={img} alt="" className="m-2" />
-    </div>
-  );
-};
-
-export default Card;
